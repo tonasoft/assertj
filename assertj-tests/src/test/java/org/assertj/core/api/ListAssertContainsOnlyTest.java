f26c00c23209810a53557b4b9ae10be656cfee03
tonasoft <tobinna4@gmail.com>
2025-03-29 19:57:07 -0400

    Add equivalence partitioning test for containsOnly in List assertions

--- assertj-tests/src/test/java/org/assertj/core/api/ListAssertContainsOnlyTest.java
+++ assertj-tests/src/test/java/org/assertj/core/api/ListAssertContainsOnlyTest.java
@@ -1,4 +1,30 @@
-package org.assertj.core.api;
+package org.assertj.tests;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 public class ListAssertContainsOnlyTest {
+
+  // Valid partition: matches expected elements
+  @Test
+  void testContainsOnlyValidPartition() {
+    List<Integer> actual = List.of(1, 2, 3);
+    Assertions.assertThat(actual).containsOnly(1, 2, 3);
+  }
+
+  // Invalid partition: has an extra element
+  @Test
+  void testContainsOnlyInvalidPartition() {
+    List<Integer> actual = List.of(1, 2, 3, 4);
+    Assertions.assertThat(actual).doesNotContain(5);
+  }
+
+  // Valid partition with duplicates
+  @Test
+  void testContainsOnlyWithDuplicates() {
+    List<Integer> actual = List.of(1, 1, 2, 3);
+    Assertions.assertThat(actual).containsOnly(1, 2, 3);
+  }
 }
